77c6411efb3acd8ec58fcbaaba29fd5e9dee9a19
jingming295 <jingming295@gmail.com>
2024-02-16 15:11:00 +0800

    fix: nickname, username, userid

--- src/split/roll.ts
+++ src/split/roll.ts
@@ -384,8 +384,8 @@ export async function rh(ctx: Context, session: Session, who?: string) {
         var prom = await session.bot.getGuildMember(session.guildId, qq[1]).then(res => res)
         // 解析
         //  var nick = prom.nickname == "" ? prom.username : prom.nickname
-        var nick = prom.nick == "" ? prom.user : prom.nick
-        var card = await getCard_who(ctx, /** prom.userId */session.guildId, session.guildId).then(res => res)
+        var nick = prom.user.nick == "" ? prom.user.name : prom.user.nick
+        var card = await getCard_who(ctx, /** prom.userId */prom.user.id, session.guildId).then(res => res)
         var num = card[1]["心理学"]
 
         var resu = r_check(num, rules)
